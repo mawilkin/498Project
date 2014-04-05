85da011f4d91d56da6fd484f78b2623bb207a348
Patrick Wilson <pdubslax@umich.edu>
2014-04-05 13:30:24 -0400

    should run until we get a real emotion

--- query2topic_emotion.py
+++ query2topic_emotion.py
@@ -7,30 +7,28 @@ def findstuffaboutquery(query):
   emotionlist = ['anger_loathing', 'enjoyment_elation',
     'amusement_excitement', 'contentment_gratitude', 'humiliation_shame',
     'fear_uneasiness', 'affection_friendliness', 'sadness_grief']
-
-
-
-  dic = lymbix.tonalize(query)
-  sentiment = example.giveMeSentiment(query)
-
-  emotion =  dic["dominant_emotion"]
-  clarity =  dic ["clarity"]
-  sentimentPosOrNeg = sentiment['sentiment']['docSentiment']['type']
-  sentimentScore = sentiment['sentiment']['docSentiment']['score']
-  mainfocusWord = sentiment['swag']
-
-  high = float(0)
-  domEmotion = emotion
-  for emotion in emotionlist:
-    num = float(dic[emotion])
-    if math.fabs(num)>high:
-      high = math.fabs(num)
-      domEmotion = emotion
   returnDic = {}
-  returnDic["emotion"]=domEmotion
-  returnDic["focus"]=mainfocusWord
-  print domEmotion
-  print mainfocusWord
-  return returnDic
+  domEmotion=='Neutral'
+  while domEmotion=='Neutral':
+    dic = lymbix.tonalize(query)
+    sentiment = example.giveMeSentiment(query)
 
-findstuffaboutquery("I am sad that today is the second to last welcome wednesday")
+    emotion =  dic["dominant_emotion"]
+    clarity =  dic ["clarity"]
+    sentimentPosOrNeg = sentiment['sentiment']['docSentiment']['type']
+    sentimentScore = sentiment['sentiment']['docSentiment']['score']
+    mainfocusWord = sentiment['swag']
+
+    high = float(0)
+    domEmotion = emotion
+    for emotion in emotionlist:
+      num = float(dic[emotion])
+      if math.fabs(num)>high:
+        high = math.fabs(num)
+        domEmotion = emotion
+
+    returnDic["emotion"]=domEmotion
+    returnDic["focus"]=mainfocusWord
+    print domEmotion
+    print mainfocusWord
+  return returnDic
